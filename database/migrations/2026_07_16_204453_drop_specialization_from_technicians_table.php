d1d33efa34e922a31dda1e30be3c91215b051d60
jaa <rezaandikatama20@gmail.com>
2026-07-16 20:46:24 +0700

    menghapus kolom specialization

--- database/migrations/2026_07_16_204453_drop_specialization_from_technicians_table.php
+++ database/migrations/2026_07_16_204453_drop_specialization_from_technicians_table.php
@@ -1,20 +1,22 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-public function up(): void
-{
-Schema::table('technicians', function (Blueprint $table) {
-$table->dropColumn('specialization');
-});
-}
+    public function up(): void
+    {
+        Schema::table('technicians', function (Blueprint $table) {
+            $table->dropColumn('specialization');
+        });
+    }
 
-public function down(): void
-{
-Schema::table('technicians', function (Blueprint $table) {
-$table->string('specialization')->nullable();
-});
-}
+    public function down(): void
+    {
+        Schema::table('technicians', function (Blueprint $table) {
+            $table->string('specialization')->nullable();
+        });
+    }
 };
